c96f0cc4b9a5b7b169fd1d89b63cad40d44121f6
Martin Thusgaard Frederiksen <1578-au606080@users.noreply.gitlab.au.dk>
2021-05-05 11:09:44 +0200

    client site updated

--- client/script.js
+++ client/script.js
@@ -1,16 +1,21 @@
 const API_URL = "http://hp-api.herokuapp.com/api/characters"
-const API_URL = "http://localhost:3000/api/v1/data"
+//const API_URL = "http://localhost:3000/api/v1/data"
+
+const dataList = [];
 
 getData()
-    .then(showData);
+    .then(DisplayData);
 
 function getData() {
     return fetch(API_URL)
     .then(res => res.json());
 }
 
-function showData(data) {
-    data.forEach(data => {
-        document.getElementById("test").innerHTML += data.name +"<br>";
+function DisplayData(data) {
+    dataList = data;
+    dataList.forEach(data => {
+        document.getElementById("data").innerHTML += data.name + "<br>";
     });
 }
+
+//DisplayData();
